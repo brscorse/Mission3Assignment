fe73173e65e364baeefb9aa960da365991b34247
brscorse <brscorse@byu.edu>
2022-01-17 10:06:28 -0700

    Mission #3 Assignment Version 1

---
+++ Mission1Assignment/Controllers/SubViewsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission3Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission3Assignment.Controllers
+{
+    public class SubViewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalc ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalc (GradeCalcModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission1Assignment/Models/GradeCalcModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission3Assignment.Models
+{
+    public class GradeCalcModel
+    {
+        // The following creates a model template based off the form in my GradeCalc.cshtml
+        //My validation is based on decimal input, so instead of a range between 0 and 100, my code implements a range between 0 and 1
+        [Range(0.0, 1.0)]
+        public float Assignment { get; set; }
+        [Range(0.0, 1.0)]
+        public float Group { get; set; }
+        [Range(0.0, 1.0)]
+        public float Quizzes { get; set; }
+        [Range(0.0, 1.0)]
+        public float Exam { get; set; }
+        [Range(0.0, 1.0)]
+        public float Intex { get; set; }
+
+    }
+}
